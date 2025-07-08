7340d4b54eb9cd97dac0c80bc37bc1889708c362
joebarbs14 <78578562+joebarbs14@users.noreply.github.com>
2025-07-08 21:45:30 +1000

    Update app.py

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'changeme')
 CORS(app)
 db.init_app(app)
