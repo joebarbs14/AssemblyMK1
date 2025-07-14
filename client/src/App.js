6e427aeb47e3c753834d28b9df6ef0623cc2f561
joebarbs14 <78578562+joebarbs14@users.noreply.github.com>
2025-07-14 23:00:11 +1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // ✅ Use HashRouter
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
